9aec8b676134c2fdf2dc42f47180157a56bce73c
Ana <jhosselyn7@gmail.com>
2020-10-29 18:51:10 -0500

    DtoUsuario_pk

---
+++ DTO/DtoUsuario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    public class DtoUsuario
+    {
+        public string PK_VU_Dni { get; set; }
+    }
+}
