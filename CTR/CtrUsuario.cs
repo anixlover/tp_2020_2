593ad1ae8122729b128a50e5959378aac1aeaf14
mirko rojas zuzunaga <mirkorojas12@hotmail.com>
2020-11-03 13:13:11 -0500

    Registrar Cliente sin front :"(

---
+++ CTR/CtrUsuario.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using DAO;
+using DTO;
+
+namespace CTR
+{
+    class CtrUsuario
+    {
+        DaoUsuario objDaoUsuario;
+        public CtrUsuario()
+        {
+            objDaoUsuario = new DaoUsuario();
+        }
+        public void RegistrarClienteUsuarioExterno(DtoUsuario objUsuario)
+        {
+            bool correcto = true;
+            string usuarioNom = "";
+            string usuarioApe = "";
+            string usuarioCor = "";
+            string usuarioDni = "";
+            string Expression = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+            int usuarioCel = 0;
+            //DNI solo tiene números
+            try
+            {
+                usuarioDni = objUsuario.PK_VU_Dni;
+                for (int i = 0; i < usuarioDni.Trim().Length; i++)
+                {
+                    correcto = char.IsDigit(usuarioNom.Trim()[i]);
+                }
+            }
+            catch
+            {
+                correcto = false;
+            }
+            if (!correcto) 
+            {
+                objUsuario.IU_Estado = 1;
+                return;
+            }
+            //Nombre solo tiene letras
+            try
+            {
+                usuarioNom = objUsuario.VU_Nombre;
+                for (int i = 0; i < usuarioNom.Trim().Length; i++)
+                {
+                    correcto = char.IsLetter(usuarioNom.Trim()[i]);
+                }
+            }
+            catch
+            {
+                correcto = false;
+            }
+            if (!correcto) 
+            {
+                objUsuario.IU_Estado = 2;
+                return;
+            }
+            //Apellido solo tiene letras
+            try
+            {
+                usuarioApe = objUsuario.VU_Apellidos;
+                for (int i = 0; i < usuarioApe.Trim().Length; i++)
+                {
+                    correcto = char.IsLetter(usuarioApe.Trim()[i]);
+                }
+            }
+            catch
+            {
+                correcto = false;
+            }
+            if (!correcto) 
+            {
+                objUsuario.IU_Estado = 3;
+                return;
+            }
+            //Correo escrito correctamente
+            usuarioCor = objUsuario.VU_Correo;
+            correcto = Regex.IsMatch(usuarioCor,Expression);
+            if (!correcto) 
+            {
+                objUsuario.IU_Estado = 4;
+                return;
+            }
+            //Dni duplicado
+            try
+            {
+                usuarioDni = objUsuario.PK_VU_Dni;
+                correcto = !objDaoUsuario.SelectUsuarioxDni(objUsuario);
+            }
+            catch
+            {
+                correcto = false;
+            }
+            if (!correcto) 
+            {
+                objUsuario.IU_Estado = 5;
+                return;
+            }
+            //Celular Duplicado
+            try
+            {
+                usuarioCel = objUsuario.IU_Celular;
+                correcto = !objDaoUsuario.SelectUsuarioxCelular(objUsuario);
+            }
+            catch
+            {
+                correcto = false;
+            }
+            if (!correcto) 
+            {
+                objUsuario.IU_Estado = 6; 
+                return; 
+            }
+            //Correo duplicado
+            try
+            {
+                usuarioCor = objUsuario.VU_Correo;
+                correcto = !objDaoUsuario.SelectUsuarioxCorreo(objUsuario);
+            }
+            catch
+            {
+                correcto = false;
+            }
+            if (!correcto) 
+            {
+                objUsuario.IU_Estado = 7; 
+                return; 
+            }
+            //Registro exitoso!
+            objUsuario.IU_Estado = 77;
+            objDaoUsuario.InsertUsuarioCliente(objUsuario);
+        }
+        public string GenerarContraseña(DtoUsuario objUsuario) 
+        {
+            DateTime fecha = new DateTime();
+            string contraseña = fecha.Year.ToString() + objUsuario.PK_VU_Dni.Trim()[0]+ objUsuario.PK_VU_Dni.Trim()[7] + fecha.Month.ToString();
+            return contraseña;
+        }
+        public void RegistrarClienteVendedor(DtoUsuario objUsuario)
+        {
+            bool correcto = true;
+            string usuarioNom = "";
+            string usuarioApe = "";
+            string usuarioCor = "";
+            string usuarioDni = "";
+            string Expression = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+            int usuarioCel = 0;
+            //DNI solo tiene números
+            try
+            {
+                usuarioDni = objUsuario.PK_VU_Dni;
+                for (int i = 0; i < usuarioDni.Trim().Length; i++)
+                {
+                    correcto = char.IsDigit(usuarioNom.Trim()[i]);
+                }
+            }
+            catch
+            {
+                correcto = false;
+            }
+            if (!correcto)
+            {
+                objUsuario.IU_Estado = 1;
+                return;
+            }
+            //Nombre solo tiene letras
+            try
+            {
+                usuarioNom = objUsuario.VU_Nombre;
+                for (int i = 0; i < usuarioNom.Trim().Length; i++)
+                {
+                    correcto = char.IsLetter(usuarioNom.Trim()[i]);
+                }
+            }
+            catch
+            {
+                correcto = false;
+            }
+            if (!correcto)
+            {
+                objUsuario.IU_Estado = 2;
+                return;
+            }
+            //Apellido solo tiene letras
+            try
+            {
+                usuarioApe = objUsuario.VU_Apellidos;
+                for (int i = 0; i < usuarioApe.Trim().Length; i++)
+                {
+                    correcto = char.IsLetter(usuarioApe.Trim()[i]);
+                }
+            }
+            catch
+            {
+                correcto = false;
+            }
+            if (!correcto)
+            {
+                objUsuario.IU_Estado = 3;
+                return;
+            }
+            //Correo escrito correctamente
+            usuarioCor = objUsuario.VU_Correo;
+            correcto = Regex.IsMatch(usuarioCor, Expression);
+            if (!correcto)
+            {
+                objUsuario.IU_Estado = 4;
+                return;
+            }
+            //Dni duplicado
+            try
+            {
+                usuarioDni = objUsuario.PK_VU_Dni;
+                correcto = !objDaoUsuario.SelectUsuarioxDni(objUsuario);
+            }
+            catch
+            {
+                correcto = false;
+            }
+            if (!correcto)
+            {
+                objUsuario.IU_Estado = 5;
+                return;
+            }
+            //Celular Duplicado
+            try
+            {
+                usuarioCel = objUsuario.IU_Celular;
+                correcto = !objDaoUsuario.SelectUsuarioxCelular(objUsuario);
+            }
+            catch
+            {
+                correcto = false;
+            }
+            if (!correcto)
+            {
+                objUsuario.IU_Estado = 6;
+                return;
+            }
+            //Correo duplicado
+            try
+            {
+                usuarioCor = objUsuario.VU_Correo;
+                correcto = !objDaoUsuario.SelectUsuarioxCorreo(objUsuario);
+            }
+            catch
+            {
+                correcto = false;
+            }
+            if (!correcto)
+            {
+                objUsuario.IU_Estado = 7;
+                return;
+            }
+            //Registro exitoso!
+            objUsuario.VU_Contraseña = GenerarContraseña(objUsuario);
+            objUsuario.IU_Estado = 77;
+            objDaoUsuario.InsertUsuarioCliente(objUsuario);
+        }
+    }
+}
---
+++ DAO/DaoUsuario.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using DTO;
+namespace DAO
+{
+    public class DaoUsuario
+    {
+        SqlConnection conexion;
+        //constructor
+        public DaoUsuario()
+        {
+            conexion = new SqlConnection(ConexiónBD.CadenaConexión);
+        }
+        public void InsertUsuarioCliente(DtoUsuario Usuario)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "SP_Usuario_Insert";
+            cmd.Connection = conexion;
+            cmd.Parameters.AddWithValue("@DNI", Usuario.PK_VU_Dni);
+            cmd.Parameters.AddWithValue("@Nom", Usuario.VU_Nombre);
+            cmd.Parameters.AddWithValue("@Ape", Usuario.VU_Apellidos);
+            cmd.Parameters.AddWithValue("@Cel", Usuario.IU_Celular);            
+            cmd.Parameters.AddWithValue("@FechaNac", Usuario.DTU_FechaNac);
+            cmd.Parameters.AddWithValue("@Cor", Usuario.VU_Correo);
+            cmd.Parameters.AddWithValue("@Con", Usuario.VU_Contraseña);
+            conexion.Open();
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
+        public bool SelectUsuarioxDni(DtoUsuario Usuario)
+        {
+            string select = "SELECT * FROM T_Usuario WHERE PK_VU_Dni = '" +Usuario.PK_VU_Dni+"'" ;
+            SqlCommand Comando = new SqlCommand(select,conexion);
+            conexion.Open();
+            SqlDataReader reader = Comando.ExecuteReader();
+            bool hayRegistros = reader.Read();
+            if (hayRegistros)
+            {
+                Usuario.VU_Nombre = (string)reader[1];
+                Usuario.VU_Apellidos = (string)reader[2];
+                Usuario.IU_Celular = (int)reader[3];
+                Usuario.VU_Correo = (string)reader[5];
+            }
+            conexion.Close();
+            return hayRegistros;
+        }
+        public bool SelectUsuarioxCorreo(DtoUsuario Usuario)
+        {
+            string select = "SELECT * FROM T_Usuario WHERE VU_Correo = '" + Usuario.VU_Correo + "'";
+            SqlCommand Comando = new SqlCommand(select, conexion);
+            conexion.Open();
+            SqlDataReader reader = Comando.ExecuteReader();
+            bool hayRegistros = reader.Read();
+            if (hayRegistros)
+            {
+                Usuario.PK_VU_Dni = (string)reader[0];
+                Usuario.VU_Nombre = (string)reader[1];
+                Usuario.VU_Apellidos = (string)reader[2];
+                Usuario.IU_Celular = (int)reader[3];
+            }
+            conexion.Close();
+            return hayRegistros;
+        }
+        public bool SelectUsuarioxCelular(DtoUsuario Usuario)
+        {
+            string select = "SELECT * FROM T_Usuario WHERE IU_Celular = '" + Usuario.IU_Celular + "'";
+            SqlCommand Comando = new SqlCommand(select, conexion);
+            conexion.Open();
+            SqlDataReader reader = Comando.ExecuteReader();
+            bool hayRegistros = reader.Read();
+            if (hayRegistros)
+            {
+                Usuario.PK_VU_Dni = (string)reader[0];
+                Usuario.VU_Nombre = (string)reader[1];
+                Usuario.VU_Apellidos = (string)reader[2];
+                Usuario.VU_Correo = (string)reader[5];
+            }
+            conexion.Close();
+            return hayRegistros;
+        }
+    }
+}
