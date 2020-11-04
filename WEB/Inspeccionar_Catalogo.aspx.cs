1406c57d06ef4444e7d3443d2b680e542d9bc322
Alvarod12345 <alvarosantisteban56@gmail.com>
2020-11-04 17:10:02 -0500

    Inspeccionar catalogo 40%

---
+++ WEB/Inspeccionar_Catalogo.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WEB
+{
+    public partial class Inspeccionar_Catalogo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTodos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBaquetonClasico_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBaquetonDecorado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRosetonClasico_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRosetonDecorado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCornisaClasica_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCornisaDecorada_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnPlaca3D_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
