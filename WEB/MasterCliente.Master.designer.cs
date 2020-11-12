1b9f90f24f3dc39cc1b2c1bb2378ae5f9aeb82be
Ana <jhosselyn7@gmail.com>
2020-11-11 23:44:36 -0500

    TMR :'v

--- WEB/MasterCliente.Master.designer.cs
+++ WEB/MasterCliente.Master.designer.cs
@@ -33,6 +33,15 @@ namespace WEB
         protected global::System.Web.UI.WebControls.ContentPlaceHolder cph_Js;
 
         /// <summary>
+        /// Control Literal1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal Literal1;
+
+        /// <summary>
         /// Control form1.
         /// </summary>
         /// <remarks>
