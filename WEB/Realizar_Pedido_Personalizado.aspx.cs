d9caa096c13fbabbeae60578b509d0a059bee40e
Ana <jhosselyn7@gmail.com>
2020-12-04 21:58:05 -0500

    push

---
+++ WEB/Realizar_Pedido_Personalizado.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WEB
+{
+    public partial class Realizar_Pedido_Personalizado : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //protected void rbCatalogo_CheckedChanged(object sender, EventArgs e)
+        //{
+        //    ddlRUC.Visible = false;
+        //    chbNuevoRUC.Visible = false;
+        //    chbNuevoRUC.Checked = false;
+        //    lblTitulo1.Visible = false;
+        //}
+
+        //protected void rbDiseñoPropio_CheckedChanged(object sender, EventArgs e)
+        //{
+        //    ddlRUC.Visible = true;
+        //    lblTitulo1.Visible = true;
+        //    chbNuevoRUC.Visible = true;
+        //}
+
+    }
+}
