7b59b2f9ba98883b3181e1adf8d50a4f74841daf
Alvarod12345 <alvarosantisteban56@gmail.com>
2020-12-08 17:18:18 -0500

    update

--- WEB/IniciarSesion.aspx.cs
+++ WEB/IniciarSesion.aspx.cs
@@ -17,6 +17,7 @@ namespace WEB
         SqlConnection conexion;
         DtoUsuario objUsuario = new DtoUsuario();
         CtrUsuario objCtrUsuario = new CtrUsuario();
+        //NO TOCAR EXPLOTA
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
