59853589555762463fdad07e462968b3a37b5536
Ana <jhosselyn7@gmail.com>
2020-12-17 10:33:45 -0500

    falta back :v

--- WEB/Realizar_Pedido_Personalizado.aspx.cs
+++ WEB/Realizar_Pedido_Personalizado.aspx.cs
@@ -4,30 +4,67 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+using DTO;
+using CTR;
+using DAO;
+using System.Configuration;
+
+using System.Data.SqlClient;
+using System.Drawing;
 
 namespace WEB
 {
     public partial class Realizar_Pedido_Personalizado : System.Web.UI.Page
     {
+        CtrMoldura objCtrMoldura = new CtrMoldura();
+        DtoMoldura objDtoMoldura = new DtoMoldura();
+        DtoTipoMoldura objDtoTipoMoldura = new DtoTipoMoldura();
+        CtrTipoMoldura objctrtipomoldura = new CtrTipoMoldura();
+        //CtrMolduraxUsuario objCtrMXU = new CtrMolduraxUsuario();
+        //DtoMolduraxUsuario objDtoMXU = new DtoMolduraxUsuario();
+        DtoSolicitud objDtoSolicitud = new DtoSolicitud();
+        //Ctr_Solicitud objCtrSolicitud = new Ctr_Solicitud();
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
-        //protected void rbCatalogo_CheckedChanged(object sender, EventArgs e)
-        //{
-        //    ddlRUC.Visible = false;
-        //    chbNuevoRUC.Visible = false;
-        //    chbNuevoRUC.Checked = false;
-        //    lblTitulo1.Visible = false;
-        //}
+        protected void btnRegistrar_Click(object sender, EventArgs e)
+        {
 
-        //protected void rbDiseñoPropio_CheckedChanged(object sender, EventArgs e)
-        //{
-        //    ddlRUC.Visible = true;
-        //    lblTitulo1.Visible = true;
-        //    chbNuevoRUC.Visible = true;
-        //}
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_BuscarProducto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalcularP_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegistrarP_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegresarP_Click(object sender, EventArgs e)
+        {
+
+        }
 
     }
 }
