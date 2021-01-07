b89ce033d7ea44681d8e225b1abe89dd85c0521d
andre ramirez <black56717@gmail.com>
2021-01-07 18:57:05 -0500

    100

--- WEB/ReporteVentas.aspx.cs
+++ WEB/ReporteVentas.aspx.cs
@@ -4,8 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
 using DTO;
 using CTR;
+using System.IO;
 
 namespace WEB
 {
@@ -44,5 +50,41 @@ namespace WEB
                 return;
             }
         }
+
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+            /* Confirms that an HtmlForm control is rendered for the specified ASP.NET
+               server control at run time. */
+        }
+
+        public override bool EnableEventValidation
+        {
+            get { return false; }
+            set { }
+        }
+
+        protected void ExportToPDF(object sender, EventArgs e)
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                using (HtmlTextWriter hw = new HtmlTextWriter(sw))
+                {
+                    gvVentas.RenderControl(hw);
+                    StringReader sr = new StringReader(sw.ToString());
+                    Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+                    PdfWriter writer = PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+                    pdfDoc.Open();
+                    XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                    pdfDoc.Close();
+
+                    Response.ContentType = "application/pdf";
+                    Response.AddHeader("content-disposition", "attachment;filename=GridViewExport.pdf");
+                    Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                    Response.Write(pdfDoc);
+                    Response.End();
+                }
+            }
+        }
+
     }
 }
